ca4ca297b121a8cb4621641a1d50f28df773ac85
MohamedAlromaihi <alromaihi2224@gmail.com>
2024-09-26 00:59:29 +0300

    i finished my HomeWork & sorry 4 my delay

---
+++ const.js
@@ -0,0 +1,17 @@
+export default {      EXERCISE__ISCHARAVOWEL: 'Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:',    EXERCISE__ISCHARAVOWEL: 'Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true'}
